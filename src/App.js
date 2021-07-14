24fb220523aaef087dfd20d675713c6722231eed
bhagyaraj92 <rajravuru92@gmail.com>
2021-07-14 12:04:33 +0000

    conditionalrendering

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: true,
+  }
+
+  //   renderAuthButton = () => {
+  //     const {isLoggedIn} = this.state
+
+  //     if (isLoggedIn === true) {
+  //       return <button>Logout</button>
+  //     }
+  //     return <button>Login</button>
+  //   }
+
+  render() {
+    const {isLoggedIn} = this.state
+    // let authButton
+    // if (isLoggedIn === true) {
+    //   authButton = <button>Logout</button>
+    // } else {
+    //   authButton = <button>Login</button>
+    // }
+    return (
+      <div className="container">
+        {<Welcome greeting="hai" />}
+        {/* <Welcome greeting="Hello" name="User" /> */}
+
+        {/* <button>Login</button>
+        <button>Logout</button> */}
+        {/* {this.renderAuthButton()} */}
+        {/* {authButton} */}
+        {/* {isLoggedIn ? <button>Logout</button> : <button>Login</button>} */}
+        {/* {isLoggedIn ? <button>Logout</button> : null} */}
+        {isLoggedIn && <button>Logout</button>}
+        {!isLoggedIn && <button>Login</button>}
+      </div>
+    )
+  }
+}
+
+export default App
